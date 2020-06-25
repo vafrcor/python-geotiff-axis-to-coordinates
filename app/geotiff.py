aa7e94a9e6755a991f49f032e4a70d0224e73573
Vinsensius Angelo <vafrcor2007@yahoo.co.id>
2020-06-25 14:01:09 +0700

    refactor code due to multi point

--- app/geotiff.py
+++ app/geotiff.py
@@ -7,6 +7,35 @@ from .utils import *
 class GeoTiffProcessor():
 
   @staticmethod
+  def read_coordinate(ds=None, transformPoint=None, x=0, y=0):
+    xoffset, px_w, rot1, yoffset, px_h, rot2 = ds.GetGeoTransform()
+
+    # This is how to get the coordinate in space.
+    posX = px_w * x + rot1 * y + xoffset
+    posY = rot2 * x + px_h * y + yoffset
+
+    # Shift to the center of the pixel
+    posX += px_w / 2.0
+    posY += px_h / 2.0
+      
+    # Get lat-long coordinates
+    (lat, long, z) = transformPoint.TransformPoint(posX, posY)
+
+    return {
+      'lat': lat,
+      'long': long,
+      'z': z,
+      'posX': posX,
+      'posY': posY,
+      'xoffset': xoffset,
+      'px_w': px_w,
+      'rot1': rot1,
+      'yoffset': yoffset,
+      'px_h': px_h,
+      'rot2': rot2
+    }
+
+  @staticmethod
   def get_axis_point_coordinate(filepath=None, x=0, y=0, opt={}):
     # References: https://stackoverflow.com/questions/50191648/gis-geotiff-gdal-python-how-to-get-coordinates-from-pixel
 
@@ -19,46 +48,29 @@ class GeoTiffProcessor():
     file_fullpath= os.path.join(BASE_DIR, filepath)
     file_size= get_file_size(file_fullpath, SIZE_UNIT.MB)
     ds = gdal.Open(file_fullpath) 
-    xoffset, px_w, rot1, yoffset, px_h, rot2 = ds.GetGeoTransform()
-
-    # is how to get the coordinate in space.
-    posX = px_w * x + rot1 * y + xoffset
-    posY = rot2 * x + px_h * y + yoffset
-
-    # shift to the center of the pixel
-    posX += px_w / 2.0
-    posY += px_h / 2.0
-
-    # get CRS from dataset 
     crs = osr.SpatialReference()
+    # get CRS from dataset 
     crs.ImportFromWkt(ds.GetProjectionRef())
     # create lat/long crs with WGS84 datum
     crsGeo = osr.SpatialReference()
     crsGeo.ImportFromEPSG(4326) # 4326 is the EPSG id of lat/long crs 
-    t = osr.CoordinateTransformation(crs, crsGeo)
-    (lat, long, z) = t.TransformPoint(posX, posY)
+    tp = osr.CoordinateTransformation(crs, crsGeo)
+    coords= GeoTiffProcessor.read_coordinate(ds, tp, x, y)
 
-    ret={
+    info={
       'filepath': file_fullpath,
       'filesize': str(file_size)+' MB',
       'x': x,
-      'y': y,
-      'posX': posX,
-      'posY': posY,
-      'lat': lat,
-      'long': long,
-      'z': z,
-      'xoffset': xoffset,
-      'px_w': px_w,
-      'rot1': rot1,
-      'yoffset': yoffset,
-      'px_h': px_h,
-      'rot2': rot2
+      'y': y
     }
 
+    ret = {**info, **coords}
+    
     if debug:
       print('DEBUG: ')
       print(json.dumps(ret, indent=4))
 
     del ds, crs
     return ret
+
+  
